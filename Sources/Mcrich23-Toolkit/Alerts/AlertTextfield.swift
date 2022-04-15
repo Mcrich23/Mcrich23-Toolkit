401553a7423a3cf4416dd52ccf98caf78d4cf6de
Morris Richman <mcrich23@icloud.com>
2022-04-15 09:59:13 -0700

    Added additional Initializers to AlertTextfield

--- Sources/Mcrich23-Toolkit/Alerts/AlertTextfield.swift
+++ Sources/Mcrich23-Toolkit/Alerts/AlertTextfield.swift
@@ -18,8 +18,6 @@ public struct AlertTextfield: View {
     @State var enablesReturnKeyAutomatically: Bool
     @State var disableAutocorrection: Bool
     @State var autocapitalization: UITextAutocapitalizationType
-//    @State var font: Font
-//    @State var fontWeight: Font.Weight
     @State var keyboardType: UIKeyboardType
     @State var returnKeyType: UIReturnKeyType
     @State var isSecureTextEntry: Bool
@@ -31,8 +29,6 @@ public struct AlertTextfield: View {
         self.enablesReturnKeyAutomatically = enablesReturnKeyAutomatically
         self.disableAutocorrection = disableAutocorrection
         self.autocapitalization = autocapitalization
-//        self.font = font
-//        self.fontWeight = fontWeight
         self.keyboardType = keyboardType
         self.returnKeyType = returnKeyType
         self.isSecureTextEntry = isSecureTextEntry
@@ -50,6 +46,57 @@ public struct AlertTextfield: View {
             .autocapitalization(autocapitalization)
 //            .font(font, weight: fontWeight)
             .keyboardType(keyboardType)
+    }
+}
+// MARK: Additional Initializers
+extension AlertTextfield {
+    public init(text: Binding<String>, placeholder: String) {
+        self._text = text
+        self.placeholder = placeholder
+        self.clearButtonMode = .whileEditing
+        self.enablesReturnKeyAutomatically = true
+        self.disableAutocorrection = false
+        self.autocapitalization = .sentences
+        self.keyboardType = .default
+        self.returnKeyType = .default
+        self.isSecureTextEntry = false
+        self.dismissKeyboardOnReturn = true
+    }
+    public init(text: Binding<String>, placeholder: String, autocapitalization: UITextAutocapitalizationType, disableAutocorrection: Bool) {
+        self._text = text
+        self.placeholder = placeholder
+        self.clearButtonMode = .whileEditing
+        self.enablesReturnKeyAutomatically = true
+        self.disableAutocorrection = disableAutocorrection
+        self.autocapitalization = autocapitalization
+        self.keyboardType = .default
+        self.returnKeyType = .default
+        self.isSecureTextEntry = false
+        self.dismissKeyboardOnReturn = true
+    }
+    public init(text: Binding<String>, placeholder: String, isSecureTextEntry: Bool) {
+        self._text = text
+        self.placeholder = placeholder
+        self.clearButtonMode = .whileEditing
+        self.enablesReturnKeyAutomatically = true
+        self.disableAutocorrection = false
+        self.autocapitalization = .sentences
+        self.keyboardType = .default
+        self.returnKeyType = .default
+        self.isSecureTextEntry = isSecureTextEntry
+        self.dismissKeyboardOnReturn = true
+    }
+    public init(text: Binding<String>, placeholder: String, keyboardType: UIKeyboardType) {
+        self._text = text
+        self.placeholder = placeholder
+        self.clearButtonMode = .whileEditing
+        self.enablesReturnKeyAutomatically = true
+        self.disableAutocorrection = false
+        self.autocapitalization = .sentences
+        self.keyboardType = keyboardType
+        self.returnKeyType = .default
+        self.isSecureTextEntry = false
+        self.dismissKeyboardOnReturn = true
     }
 }
 #endif
