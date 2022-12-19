b0cabb3e80123155d8aa3c32083f5bceae9ad773
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2022-12-19 11:15:13 -0800

    Added ConvertedGlyphImage

--- Sources/Mcrich23-Toolkit/Data Structs.swift
+++ Sources/Mcrich23-Toolkit/Data Structs.swift
@@ -11,8 +11,10 @@ import UIKit
 #elseif os(macOS)
 import Cocoa
 #endif
+import SwiftUI
+import URLImage
 /**
- Different types of glyphs, whether it be icons, or images. one variable for all the types.
+ Different types of glyphs, whether it be icons, or images. one variable for all the types. Convert it to an image with `ConvertedGlyphImage`
  */
 public enum glyphImage: Hashable, Equatable {
     /// SF Symbol
@@ -22,6 +24,60 @@ public enum glyphImage: Hashable, Equatable {
     /// Image From Remote Location
     case remoteImage(url: URL)
     case defaultIcon
+}
+
+/**
+ Converts glyphImage to a SwiftUI Image
+ 
+ - parameter glyphImage: The glyphImage
+ - parameter defaultIcon: The default icon for a view.
+ - parameter modifiers: All of the modifiers for your image.
+ - returns: Image
+ 
+ # Example #
+ ```
+ ConvertedGlyphImage(glyphImage: $glyphImage, defaultIcon: Image(systemName: "apps.iphone") { image in
+     image
+         .resizable()
+         .aspectRatio(contentMode: .fit)
+         .foregroundColor(.primary)
+ }
+ ```
+ 
+ */
+public struct ConvertedGlyphImage<Content: View, Modifier: View>: View {
+    @Binding public var glyphImage: glyphImage
+    @State public var defaultIcon: Content
+    @State public var modifiers: (Image) -> Modifier
+    public init(glyphImage: Binding<glyphImage>, defaultIcon: Content, modifiers: @escaping (Image) -> Modifier) {
+        self._glyphImage = glyphImage
+        self._defaultIcon = State(initialValue: defaultIcon)
+        self._modifiers = State(initialValue: modifiers)
+    }
+    public var body: some View {
+        HStack {
+            switch glyphImage {
+            case .systemImage(let named):
+                modifiers(Image(systemName: named))
+            case .assetImage(let named):
+                modifiers(Image(named))
+            case .remoteImage(let url):
+                if #available(iOS 15, *) {
+                    AsyncImage(url: url) { image in
+                        modifiers(image)
+                    } placeholder: {
+                        modifiers(Image(systemName: "photo"))
+                    }
+                } else {
+                    URLImage(url) { image in
+                        modifiers(image)
+                    }
+                }
+            case .defaultIcon:
+                defaultIcon
+            }
+        }
+    }
 }
 /**
 Class for all the free floating functions
