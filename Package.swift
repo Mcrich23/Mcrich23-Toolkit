77af716d52b9e9ed1d83c233a1be0b0bea4565c3
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-02-17 12:46:51 -0800

    removed unnecessary dependencies

--- Package.swift
+++ Package.swift
@@ -24,8 +24,6 @@ let package = Package(
         .package(name: "URLImage", url: "https://github.com/dmytro-anokhin/url-image", from: "3.1.0"),
         .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect", from: "0.1.3"),
         .package(name: "LoaderUI", url: "https://github.com/Mcrich23/LoaderUI", branch: "master"),
-        .package(name: "WKView", url: "https://github.com/rebeloper/WKView", branch: "main"),
-        .package(name: "MultiModal", url: "https://github.com/davdroman/MultiModal", from: "2.0.0"),
         .package(name: "SwiftUIMap", url: "https://github.com/Mcrich23/SwiftUIMap", from: "1.1.0"),
         .package(name: "StepperView", url: "https://github.com/badrinathvm/StepperView", branch: "master"),
         .package(name: "SwiftUIBackports", url: "https://github.com/shaps80/SwiftUIBackports", from: "1.6.2"),
@@ -36,7 +34,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Mcrich23-Toolkit",
-            dependencies: ["SwiftUIX", "URLImage", "Introspect", "LoaderUI", "WKView", "MultiModal", "SwiftUIMap", "StepperView", "SwiftUIBackports", "DeviceKit"]
+            dependencies: ["SwiftUIX", "URLImage", "Introspect", "LoaderUI", "SwiftUIMap", "StepperView", "SwiftUIBackports", "DeviceKit"]
             )
         ]
 )
